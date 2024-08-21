4a0d15700de26057224ad1d7a8de11fe20108ed8
Simone <simo.diricco.91@gmail.com>
2024-08-21 12:46:16 +0200

    add webhashhistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
 const HomeView = () => import('../views/Home.vue')
 const Layout1View = () => import('../views/Layout1.vue')
 const Layout2View = () => import('../views/Layout2.vue')
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', redirect: '/home' },
     { path: '/home', name: 'home', component: HomeView },
